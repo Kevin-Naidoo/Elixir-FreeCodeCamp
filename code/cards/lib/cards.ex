3fede7cb172f90adb79e6c98e056df12e0d286d4
Kevin-Naidoo <kevinnelsonnaidoo@gmail.com>
2023-07-18 16:52:07 +0200

    Expanding deck and adding suits

--- code/cards/lib/cards.ex
+++ code/cards/lib/cards.ex
@@ -1,6 +1,11 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"] #list of cards
+    values = ["Ace", "Two", "Three", "Four", "Five"] #list of cards
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"] #list of suits
+
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
