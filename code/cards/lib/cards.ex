d80e3f52c83cf1f9d2350e1a046c84e4b03c48ed
Kevin-Naidoo <kevinnelsonnaidoo@gmail.com>
2023-07-18 16:31:14 +0200

    Card contains function and comments

--- code/cards/lib/cards.ex
+++ code/cards/lib/cards.ex
@@ -1,9 +1,13 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    ["Ace", "Two", "Three"] #list of cards
   end
 
   def shuffle(deck) do
-    deck |> Enum.shuffle()
+    deck |> Enum.shuffle() #Enum operating on cards list
+  end
+
+  def contains?(deck, card) do
+    deck |> Enum.member?(card)
   end
 end
