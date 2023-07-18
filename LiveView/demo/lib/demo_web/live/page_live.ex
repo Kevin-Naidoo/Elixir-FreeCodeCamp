1496c55405ddfceb951f7265984d69071c7128fc
Kevin-Naidoo <kevinnelsonnaidoo@gmail.com>
2023-07-18 13:21:55 +0200

    Phoenix basic site following the Phoenix 1.7 LiveView Fundamentals course on Udemy

---
+++ LiveView/demo/lib/demo_web/live/page_live.ex
@@ -0,0 +1,20 @@
+defmodule DemoWeb.PageLive do
+  use DemoWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, number: 0)}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <%= @number %>
+    <strong> Hello World</strong>
+    <.button phx-click="add">Add</.button>
+    """
+  end
+
+  def handle_event("add", _params, socket) do
+    {:noreply, assign(socket, number: socket.assigns.number + 1)}
+  end
+
+end
