a0bed95643e008c0407ad4dcfa5b6eecbd8f0537
Kevin-Naidoo <kevinnelsonnaidoo@gmail.com>
2023-07-20 07:22:25 +0200

    Building grid and mirroring function

--- code/identicon/lib/identicon.ex
+++ code/identicon/lib/identicon.ex
@@ -6,13 +6,27 @@ defmodule Identicon do
     input
       |> hash_input
       |> pick_color
+      |> build_grid
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk_every(3)
+  end
 
-    [r, g, b]
+  def mirror_row(row) do
+    #[145, 46, 200]
 
+    [first, second | _tail] = row
+
+    row ++ [second, first]
+
+    #[145, 46, 200 ]
+
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r,g,b}}
   end
 
   def hash_input(input) do
