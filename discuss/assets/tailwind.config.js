fe0ba8c6d1ebf7a1942410304cf8c9dfeff08af3
Kevin-Naidoo <kevinnelsonnaidoo@gmail.com>
2023-07-26 06:50:02 +0200

    Clearing home.html.heex file and printing Hello World using tailwind CSS

--- discuss/assets/tailwind.config.js
+++ discuss/assets/tailwind.config.js
@@ -14,7 +14,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        brand: "#FD4F00",
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        negative: 'var(--color-negative)',
+        positive: 'var(--color-positive)',
+        'primary-background': 'var(--background-primary)',
+        'sec-background': 'var(--background-sec)',
+        'primary-text': 'var(--color-text-primary)',
       }
     },
   },
